305c7dc689bbfc132e2f4c02a555ca2f86aae8ce
safsaf150 <sayfeddine-@hotmail.com>
2014-05-28 12:34:15 +0200

    id

--- SEFLab/functions/login.php
+++ SEFLab/functions/login.php
@@ -6,8 +6,8 @@ $tbl_name = "users";
 
 $email = mysql_real_escape_string($_POST['email']);
 
-session_start();//start session so email can be stored
-$_SESSION["usrname"] = $email ; //storing variable into session
+//session_start();//start session so email can be stored
+
 
 $password = mysql_real_escape_string($_POST['password']);
 $sql = mysql_query("SELECT `password` FROM `users` WHERE `email`='$email'")
@@ -22,6 +22,7 @@ if ($count == 1 && crypt($password, $verify['password']) == $verify['password'])
     $_SESSION['count'] = '1';
 //    $_SESSION['password']= $password;
     header("location:../views/home.php");
+	$_SESSION["usrname"] = $email ; //storing variable into session
 } else {    
     $_SESSION['count'] = '0';
     header("refresh:5; url = ../index.php");
