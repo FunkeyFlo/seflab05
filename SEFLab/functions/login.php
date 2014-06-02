87da6c57a65c099e1fbde3ef76fee9ffcd071ac0
jay.richardson@hva.nl <jay.richardson@hva.nl>
2014-06-02 14:24:33 +0200

    added id to session

--- SEFLab/functions/login.php
+++ SEFLab/functions/login.php
@@ -6,11 +6,8 @@ $tbl_name = "users";
 
 $email = mysql_real_escape_string($_POST['email']);
 
-//session_start();//start session so email can be stored
-
-
 $password = mysql_real_escape_string($_POST['password']);
-$sql = mysql_query("SELECT `password` FROM `users` WHERE `email`='$email'")
+$sql = mysql_query("SELECT `password`, `id` FROM `users` WHERE `email`='$email'")
 	or die(mysql_error());
 $result = $sql;
 $verify = mysql_fetch_array($sql);
@@ -20,9 +17,10 @@ $connection->closeConnection();
 if ($count == 1 && crypt($password, $verify['password']) == $verify['password']) {
     session_start();
     $_SESSION['count'] = '1';
+	$_SESSION['usrname'] = $email ; //storing variable into session
+	$_SESSION['id'] = $verify['id'];
 //    $_SESSION['password']= $password;
     header("location:../views/home.php");
-	$_SESSION["usrname"] = $email ; //storing variable into session
 } else {    
     $_SESSION['count'] = '0';
     header("refresh:5; url = ../index.php");
