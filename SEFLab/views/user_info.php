94d417eb450f55b2f79841b9bc3d0e8585688364
W Blankennagel <wesley.blankennagel@hva.nl>
2014-06-09 11:18:19 +0200

    Settings Pagina
    kan naam,achternaam en wachtwoord veranderen

--- SEFLab/views/user_info.php
+++ SEFLab/views/user_info.php
@@ -13,18 +13,49 @@ $connection = new Database();
 $connection->openConnection(); // connected to the database 
 $id= $_SESSION['id'];
 
+$sql = mysql_query("SELECT `id`,`firstname`,`lastname`,`password` FROM `users`  WHERE `id` ='$id'")
+ or die(mysql_error());
+$result = $sql;
+
 ?>
 
-<div class="row">
-    <div class="col-lg-12">
-	
-        <h1 class="page-header">My Info</h1>
-		<div id ="middle">
-		
-		</div>
-    <!-- /.col-lg-12 -->
-</div>
-<!-- /.row -->
+<body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    
+                    <div class="login-panel panel panel-default">
+                        
+                        <div class="panel-body">
+                            <form name="register_form" action='../functions/change_user.php' method='POST'>
+                                <fieldset>
+<?php
+while ($row = mysql_fetch_array($result)) {
+
+    echo'                           <div class="form-group">
+                                    <label>Change Password</label>
+                                    <input class="form-control" name="password"  value=>
+                                    </div>
+									
+                                    <div class="form-group">
+                                        <label>Change Firstname</label>
+                                        <input class="form-control" name="firstname" value='.$row['firstname'].'>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Change Lastname</label>
+                                        <input class="form-control" name="lastname" value='.$row['lastname'].'>
+                                    </div>	';}
+?>
+                                            <!-- Change this to a button or input when using this as a form -->
+                                            <input type="submit" value="Change info"  class="btn btn-lg btn-success btn-block"></a>
+                                            </fieldset>
+                                            </form>
+                                            </div>
+                                            </div>
+                                            </div>
+                                            </div>
+                                            </div>
 
 
 <?php
